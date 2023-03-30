7194a71befeeab47d64bcdc85bc18e71dd24dbd0
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-03-30 11:49:35 -0300

    Create TCPClient.java

---
+++ Socket/TCP/TCPClient.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+
+class TCPClient {
+
+    public static void main(String args[]) throws Exception {
+        String sentence;
+
+        String modifiedSentence;
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+                System.in));
+
+        int porta = 6789;
+
+        String servidor = "localhost";
+
+        System.out.println("Conectando ao servidor " + servidor + ":" + porta);
+
+        Socket clientSocket = new Socket(servidor, porta);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket
+                .getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
+                clientSocket.getInputStream()));
+
+        System.out.println("Digite o conteúdo a ser enviada para o servidor: ");
+        sentence = inFromUser.readLine();
+
+        outToServer.writeBytes(sentence + '\n');
+
+        modifiedSentence = inFromServer.readLine();
+
+        System.out.println("Recebido do servidor: " + modifiedSentence);
+
+        clientSocket.close();
+
+    }
+}
