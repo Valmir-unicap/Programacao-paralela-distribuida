506ae34f45bcfc590d1f29f89f8c8d60f253b204
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-03-31 12:21:49 -0300

    Update TCPClient.java

--- Socket/TCP/TCPClient.java
+++ Socket/TCP/TCPClient.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.io.*;
 import java.net.*;
 
@@ -23,7 +24,7 @@ class TCPClient {
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         // Lê uma mensagem do usuário a partir do console
-        System.out.println("Digite a mensagem a ser enviada para o servidor: ");
+        System.out.print("Digite a mensagem a ser enviada para o servidor:");
         String sentence = inFromUser.readLine();
 
         // Envia a mensagem ao servidor através do Socket
