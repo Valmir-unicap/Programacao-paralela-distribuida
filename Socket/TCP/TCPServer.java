3ab5198186b0edec0d6981c3910e6cb1ef23e278
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-03-31 12:21:31 -0300

    Update TCPServer.java

--- Socket/TCP/TCPServer.java
+++ Socket/TCP/TCPServer.java
@@ -5,12 +5,15 @@ import java.net.*;
 
 class TCPServer {
     public static void main(String args[]) throws Exception {
+
+        int contador = 0;
+
         // Cria um objeto ServerSocket na porta 6789
         ServerSocket welcomeSocket = new ServerSocket(6789);
         System.out.println("Servidor iniciado");
 
         // Aguarda a conexão de clientes indefinidamente
-        while (true) {
+        while (contador == 0) {
             // Aguarda a conexão de um cliente e cria um novo objeto Socket para lidar com ele
             Socket connectionSocket = welcomeSocket.accept();
             System.out.println("Cliente conectado: " + connectionSocket);
@@ -33,6 +36,10 @@ class TCPServer {
             // Fecha o Socket para este cliente
             connectionSocket.close();
             System.out.println("Cliente desconectado: " + connectionSocket);
+            welcomeSocket.close();
+            System.out.println("Servidor desconectado: " + welcomeSocket);
+
+            contador++;
         }
     }
 }
